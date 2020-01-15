f69d350bfbb2a753416d393f35ff289df8374999
LeuAlmeida <leo@webid.net.br>
2020-01-15 19:06:22 -0300

    chore: Incrementing mongoose configs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:devradaromnistack@cluster0-dzmtm.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
