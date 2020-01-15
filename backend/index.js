77157c064f04e2e713545dc1e061e38c151603b4
LeuAlmeida <leo@webid.net.br>
2020-01-15 18:34:09 -0300

    chore: Incrementing json format to app

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.json({ok: 'Hello lWorld'});
+  return res.json({ok: 'Hello World'});
 });
 
 app.listen(3335);
