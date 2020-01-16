588ced0cf25975bc1488eaa09ba2ee0456bba3e3
LeuAlmeida <leo@webid.net.br>
2020-01-16 14:41:09 -0300

    chore: Incrementing methods into DevController

---
+++ backend/src/controller/DevController.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const {
+      github_username, techs, latitude, longitude,
+    } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const response = await axios.get(
+        `https://api.github.com/users/${github_username}`,
+      );
+
+      const { name = login, avatar_url, bio } = response.data;
+
+      const techsArray = techs.split(',').map((tech) => tech.trim());
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      });
+    }
+    return res.json(dev);
+  },
+};
