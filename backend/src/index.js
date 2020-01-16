fadd7733157f62283cfc796e63d631f5ae256da8
LeuAlmeida <leo@webid.net.br>
2020-01-16 12:11:37 -0300

    refactor: Change routes to separated file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
@@ -10,7 +11,6 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 });
 
 app.use(express.json());
-
-app.get('/', (req, res) => res.json({ ok: 'Hello World' }));
+app.use(routes);
 
 app.listen(3335);
