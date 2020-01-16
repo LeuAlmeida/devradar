e394ce735bd7cf6371eb886af58e1c47572e6745
LeuAlmeida <leo@webid.net.br>
2020-01-16 12:23:10 -0300

    chore: Update Dev model

---
+++ backend/src/models/Dev.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const DevSchema = new mongoose.Schema({
+  name: String,
+  github_username: String,
+  bio: String,
+  avatar_url: String,
+  techs: [String],
+});
+
+module.exports = mongoose.model('Dev', DevSchema);
