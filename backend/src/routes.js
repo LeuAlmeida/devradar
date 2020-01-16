9b96d694850251a83af47b6ae4edb9fb79c50e35
LeuAlmeida <leo@webid.net.br>
2020-01-16 13:51:52 -0300

    chore: Improving routes using location

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,13 +7,22 @@ const Dev = require('./models/Dev');
 const routes = Router();
 
 routes.get('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
+  const {
+    github_username, techs, latitude, longitude,
+  } = req.body;
 
-  const response = await axios.get(`https://api.github.com/users/${github_username}`);
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`,
+  );
 
   const { name = login, avatar_url, bio } = response.data;
 
   const techsArray = techs.split(',').map((tech) => tech.trim());
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
 
   const dev = await Dev.create({
     github_username,
@@ -21,6 +30,7 @@ routes.get('/devs', async (req, res) => {
     avatar_url,
     bio,
     techs: techsArray,
+    location,
   });
 
   return res.json(dev);
