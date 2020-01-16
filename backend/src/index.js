c0e0cf2e94ba652011835dc59319e483a8b79fed
LeuAlmeida <leo@webid.net.br>
2020-01-16 13:51:21 -0300

    fix: Improving errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-dzmtm.mongodb.net/week10?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@devradar-dzmtm.mongodb.net/week10?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.use(express.json());
