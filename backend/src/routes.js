c72ddd6ca226dc83240a0baf3d9d2ce0d956ed09
LeuAlmeida <leo@webid.net.br>
2020-01-16 13:01:54 -0300

    chore: Improving routes to create dev method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,29 @@
+/* eslint-disable no-undef */
+
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.get('/devs', (req, res) => {
-  const { github_username } = req.body;
+routes.get('/devs', async (req, res) => {
+  const { github_username, techs } = req.body;
 
-  return res.json({
+  const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  const { name = login, avatar_url, bio } = response.data;
+
+  const techsArray = techs.split(',').map((tech) => tech.trim());
+
+  const dev = await Dev.create({
     github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
   });
+
+  return res.json(dev);
 });
 
 module.exports = routes;
