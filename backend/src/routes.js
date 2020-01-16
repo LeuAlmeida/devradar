5d1cb4eb3312e976d8cfc98b03d3539513090ff9
LeuAlmeida <leo@webid.net.br>
2020-01-16 14:40:49 -0300

    refactor: Remove methods into dev post route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,39 +1,10 @@
 /* eslint-disable no-undef */
 
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controller/DevController');
 
 const routes = Router();
 
-routes.get('/devs', async (req, res) => {
-  const {
-    github_username, techs, latitude, longitude,
-  } = req.body;
-
-  const response = await axios.get(
-    `https://api.github.com/users/${github_username}`,
-  );
-
-  const { name = login, avatar_url, bio } = response.data;
-
-  const techsArray = techs.split(',').map((tech) => tech.trim());
-
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  };
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-    location,
-  });
-
-  return res.json(dev);
-});
+routes.get('/devs', DevController.store);
 
 module.exports = routes;
