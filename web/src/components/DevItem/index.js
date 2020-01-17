c156281586dd0dd7e6dc931cbf0cc6dd20b5413f
LeuAlmeida <leo@webid.net.br>
2020-01-17 18:37:50 -0300

    chore: Improving DevItem component

---
+++ web/src/components/DevItem/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FaChevronRight } from 'react-icons/fa';
+
+function DevItem({ dev }) {
+  return (
+    <li className="dev-item">
+      <header>
+        <img src={dev.avatar_url} alt={dev.name} />
+
+        <div className="user-info">
+          <strong>{dev.name}</strong>
+          <span>{dev.techs.join(', ')}</span>
+        </div>
+      </header>
+      <p>{dev.bio}</p>
+      <a href={`https://github.com/${dev.github_username}`}>
+        Acessar perfil no Github <FaChevronRight color="#fff" size={12} />
+      </a>
+    </li>
+  );
+}
+
+export default DevItem;
