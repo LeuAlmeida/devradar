a52f5291a3dda9cad3f5ad8373784b455adee1c2
LeuAlmeida <leo@webid.net.br>
2020-01-17 11:25:37 -0300

    feat: Create initial sidebar structure

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,40 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 import './global.css';
+import './App.css';
 
 export default function src() {
   return (
     <div id="app">
-      <aside />
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main />
     </div>
   );
