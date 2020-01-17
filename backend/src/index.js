b48d1cea40aa12f8b3ce5a71e41c4e38a98076c2
LeuAlmeida <leo@webid.net.br>
2020-01-17 15:39:10 -0300

    chore: Update index to use Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,7 +11,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
