db22d5bfaa790ae31b7a3ccdd8ba7c3e592d83ec
LeuAlmeida <leo@webid.net.br>
2020-01-17 11:11:19 -0300

    refactor: Reset app component

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // import { Container } from './styles';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button type="button" onClick={() => incrementCounter()}>
-        Adicionar
-      </button>
-    </>
-  );
+export default function src() {
+  return <div />;
 }
-
-export default App;
