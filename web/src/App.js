ae045d0fd27bec4abfb1658930ab3ded3095ff67
LeuAlmeida <leo@webid.net.br>
2020-01-17 11:03:38 -0300

    feat: Create incrementCounter function

--- web/src/App.js
+++ web/src/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 // import { Container } from './styles';
 
 function App() {
+  function incrementCounter() {}
+
   return (
     <>
       <h1>Contador: 0</h1>
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={() => incrementCounter()}>
+        Adicionar
+      </button>
     </>
   );
 }
