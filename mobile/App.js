a0ca7d6c555d2f95af7daa852ab7a8581b8eb53b
Léu Almeida <leo@webid.net.br>
2020-01-19 12:30:14 -0300

    chore: Reset App file

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
-export default function App() {
+// import { Container } from './styles';
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <View />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
