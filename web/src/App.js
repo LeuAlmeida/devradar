f1930402c6857c92f58beb906c94f8152f816675
LeuAlmeida <leo@webid.net.br>
2020-01-20 16:41:51 -0300

    feat: Incrementing edit method into App file

--- web/src/App.js
+++ web/src/App.js
@@ -17,6 +17,8 @@ import DevForm from './components/DevForm';
 
 function App() {
   const [devs, setDevs] = useState([]);
+  const [activeEdit, setActiveEdit] = useState(false);
+  const [editDev, setEditDev] = useState({});
 
   useEffect(() => {
     async function loadDevs() {
@@ -26,12 +28,18 @@ function App() {
     }
 
     loadDevs();
-  }, []);
+  }, [handleUpdateDev]);
 
   async function handleAddDev(data) {
-    const response = await api.post('/devs', data);
+    try {
+      const response = await api.post('/devs', data);
 
-    setDevs([...devs, response.data]);
+      setDevs([...devs, response.data]);
+
+      toast('Dev cadastrado com sucesso.');
+    } catch {
+      toast.error('Erro ao cadastrar dev.');
+    }
   }
 
   async function handleRemoveDev(id) {
@@ -46,19 +54,47 @@ function App() {
     }
   }
 
+  async function handleUpdateDev(dev) {
+    const { _id, github_username, techs } = dev;
+
+    try {
+      await api.put(`/devs/${_id}`, {
+        techs: Array.isArray(techs) ? techs.join() : techs,
+        github_username,
+      });
+      setDevs([...devs]);
+      setActiveEdit(false);
+
+      toast('Dev alterado com sucesso.');
+    } catch {}
+  }
+
+  async function handleEditDev(dev) {
+    setActiveEdit(true);
+    setEditDev(dev);
+  }
+
   return (
     <div id="app">
       <aside>
         <div className="register-icon">
-          <img src={world} alt="Cadastrar" />
+          <img src={world} alt={activeEdit ? 'Editar' : 'Cadastrar'} />
         </div>
-        <strong>Cadastrar</strong>
-        <DevForm onSubmit={handleAddDev} />
+        <strong>{activeEdit ? 'Editar' : 'Cadastrar'}</strong>
+        <DevForm
+          onSubmit={activeEdit ? handleUpdateDev : handleAddDev}
+          editFields={editDev}
+        />
       </aside>
       <main>
         <ul>
           {devs.map(dev => (
-            <DevItem dev={dev} key={dev._id} onDelete={handleRemoveDev} />
+            <DevItem
+              dev={dev}
+              key={dev._id}
+              onDelete={handleRemoveDev}
+              onEdit={handleEditDev}
+            />
           ))}
         </ul>
       </main>
