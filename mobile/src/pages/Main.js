b2d861f538c6acbfb9f90a82ce6e37c3531ed64e
LeuAlmeida <leo@webid.net.br>
2020-01-21 14:53:58 -0300

    chore: Improving input styles

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -37,34 +45,52 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -23.6477446, longitude: -46.5644216 }}>
-        <Image
-          style={styles.avatar}
-          resizeMode="cover"
-          source={{
-            uri: 'https://avatars2.githubusercontent.com/u/42948574?v=4',
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -23.6477446, longitude: -46.5644216 }}>
+          <Image
+            style={styles.avatar}
+            resizeMode="cover"
+            source={{
+              uri: 'https://avatars2.githubusercontent.com/u/42948574?v=4',
+            }}
+          />
+
+          <Callout
+            onPress={() => {
+              navigation.navigate('Profile', { github_username: 'LeuAlmeida' });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Léu Almeida</Text>
+              <Text style={styles.devBio}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas, accusamus voluptate harum dolore numquam aspernatur
+                omnis inventore, culpa repellat facere consectetur deserunt aut
+                velit ut quod molestiae pariatur qui hic!
+              </Text>
+              <Text style={styles.devTechs}>
+                ReactJS, React Native, Node.js
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
 
-        <Callout
-          onPress={() => {
-            navigation.navigate('Profile', { github_username: 'LeuAlmeida' });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Léu Almeida</Text>
-            <Text style={styles.devBio}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-              accusamus voluptate harum dolore numquam aspernatur omnis
-              inventore, culpa repellat facere consectetur deserunt aut velit ut
-              quod molestiae pariatur qui hic!
-            </Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -72,26 +98,67 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+
   avatar: {
     width: 54,
     height: 54,
-    borderRadius: 35,
+    borderRadius: 27,
     borderWidth: 2,
     borderColor: '#FFF',
   },
+
   callout: {
     width: 260,
   },
+
   devName: {
     fontWeight: 'bold',
     fontSize: 16,
   },
+
   devBio: {
     color: '#666',
     marginTop: 5,
   },
+
   devTechs: {
     marginTop: 5,
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   },
 });
 
