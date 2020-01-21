ef0a844cef2c3f98426c03e8a466bc1f8500df1c
LeuAlmeida <leo@webid.net.br>
2020-01-21 13:19:06 -0300

    feat: Incrementing user location with mapview

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
 const styles = StyleSheet.create({
   map: {
@@ -9,11 +13,36 @@ const styles = StyleSheet.create({
 });
 
 function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
   useEffect(() => {
-    async function loadInitialPosition() {}
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+
+    loadInitialPosition();
   }, []);
 
-  return <MapView style={styles.map} />;
+  if (!currentRegion) {
+    return null;
+  }
+
+  return <MapView initialRegion={currentRegion} style={styles.map} />;
 }
 
 export default Main;
