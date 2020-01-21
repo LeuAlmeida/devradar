a2e613aec21562a158bee902e1395e84489012dd
LeuAlmeida <leo@webid.net.br>
2020-01-21 12:56:49 -0300

    feat: Import MapView into Main component

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 function Main() {
-  return (
-    <View>
-      <Text>Main</Text>
-    </View>
-  );
+  return <MapView style={styles.map} />;
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
 
 export default Main;
