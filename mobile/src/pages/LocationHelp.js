0b62c3436b0cabc023364035c41923cbdc9b7d78
LeuAlmeida <leo@webid.net.br>
2020-01-22 14:09:49 -0300

    chore: Initial structure of LocationHelp page

---
+++ mobile/src/pages/LocationHelp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+function LocationHelp() {
+  return <View />;
+}
+
+export default LocationHelp;
