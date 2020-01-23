72b7d192673109cc919b7a38eda25490d730452d
LeuAlmeida <leo@webid.net.br>
2020-01-23 15:07:29 -0300

    feat: Create connections database in an array to store user id and coordinates

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,9 +1,21 @@
 const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+
+const connections = [];
 
 exports.setupWebSocket = (server) => {
   const io = socketio(server);
 
   io.on('connection', (socket) => {
-    console.log(socket.id);
+    const { latitude, longitude, techs } = socket.handshake.query;
+
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+      },
+      techs: parseStringAsArray(techs),
+    });
   });
 };
