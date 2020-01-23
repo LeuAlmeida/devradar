88aa9dff186b4cfb6bc3ab78d099813b5ee60535
LeuAlmeida <leo@webid.net.br>
2020-01-23 14:26:35 -0300

    feat: Incrementing websocket on server file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
+
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
 const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@devradar-dzmtm.mongodb.net/week10?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
