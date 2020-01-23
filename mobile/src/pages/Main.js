5b1d38d2c1abd2dbcd7cdc38203ba085f940a964
LeuAlmeida <leo@webid.net.br>
2020-01-23 18:46:04 -0300

    feat: Incrmeenting function to listen new devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -13,10 +13,10 @@ import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from 'expo-location';
-import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -46,6 +46,18 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -58,6 +70,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
