acdc304433c749820c02b75f1c55d6473755d8c8
LeuAlmeida <leo@webid.net.br>
2020-01-23 18:06:18 -0300

    chore: Improving websocket configs and verifications

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,10 +1,13 @@
+/* eslint-disable max-len */
 const socketio = require('socket.io');
 const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
 
+let io;
 const connections = [];
 
 exports.setupWebSocket = (server) => {
-  const io = socketio(server);
+  io = socketio(server);
 
   io.on('connection', (socket) => {
     const { latitude, longitude, techs } = socket.handshake.query;
@@ -17,5 +20,13 @@ exports.setupWebSocket = (server) => {
       },
       techs: parseStringAsArray(techs),
     });
+  });
+};
+
+exports.findConnections = (coordinates, techs) => connections.filter((connection) => calculateDistance(coordinates, connections.coordinates) < 10 && connection.techs.some((item) => techs.includes(item)));
+
+exports.sendMessage = (to, message, data) => {
+  to.forEach((connection) => {
+    io.to(connection.id).emit(message, data);
   });
 };
